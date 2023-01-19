429fa4facc5fb6554ff39f838f4bf86c2bbddce7
Pascale.Iulian <73236565+iulian177@users.noreply.github.com>
2023-01-19 12:10:01 +0200

    Add files via upload

---
+++ _17,18,19,20/src/Main.java
@@ -0,0 +1,84 @@
+// for 20
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //16 2D array
+
+//        String[][] cars = 	{
+//                {"Camaro","Corvette","Silverado"},
+//                {"Mustang","Ranger","F-150"},
+//                {"Ferrari","Lambo","Tesla"}
+//        };
+
+		/*
+		cars[0][0] = "Camaro";
+		cars[0][1] = "Corvette";
+		cars[0][2] = "Silverado";
+		cars[1][0] = "Mustang";
+		cars[1][1] = "Ranger";
+		cars[1][2] = "F-150";
+		cars[2][0] = "Ferrari";
+		cars[2][1] = "Lambo";
+		cars[2][2] = "Tesla";
+		*/
+
+//        for(int i=0; i<cars.length; i++) {
+//            System.out.println();
+//            for(int j=0; j<cars[i].length; j++) {
+//                System.out.print(cars[i][j]+" ");
+//            }
+//        }
+
+        //17 String methods
+
+        //String name = "Bro";
+        //int result = name.indexOf("o");
+        //boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.trim();
+        //String result = name.replace('o', 'a');
+
+        //System.out.println(result);
+
+        //18 Wrapper classes
+
+        //primitive   //wrapper - have acces to methods
+        //int         //Integer
+
+//        Boolean a = true;   Boolean e = true;
+//        Character b  = '@';
+//        Integer c = 123;      int f = 5;
+//        String d = "hello";
+//
+//        System.out.println( a.compareTo(e));
+
+        //20 - 2D ArrayList
+        //ArrayList si array e ca si Integer si int
+
+//        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+//
+//        ArrayList<String> bakerylist = new ArrayList<>();
+//        bakerylist.add("pasta");
+//        bakerylist.add("garlic bread");
+//        bakerylist.add("donuts");
+//
+//        System.out.println(bakerylist);
+//        System.out.println(bakerylist.get(1));
+//
+//        ArrayList<String> produceList = new ArrayList<>();
+//        produceList.add("tomato");
+//        produceList.add("zucchini");
+//        produceList.add("peppers");
+//
+//        ArrayList<String> drinkList = new ArrayList<>();
+//        drinkList.add("soda");
+//        drinkList.add("coffee");
+//
+//        groceryList.add(bakerylist);
+//        groceryList.add(produceList);
+//        groceryList.add(drinkList);
+//        System.out.println(groceryList.get(0).get(1));
+ }
+}
